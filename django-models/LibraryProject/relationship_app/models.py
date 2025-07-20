92673add44bf1b95fe5f5cf44a07ce9b4313b60a
shimeih <shimeih@yahoo.com>
2025-07-20 23:10:40 +0200

    hfhf

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -71,3 +71,6 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+
+class Meta 
+permissions
