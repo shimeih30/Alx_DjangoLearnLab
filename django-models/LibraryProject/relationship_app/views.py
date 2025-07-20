3d744f65b266e0678961fba374019dc2885db8ae
shimeih <shimeih@yahoo.com>
2025-07-20 22:27:49 +0200

    hfhf

---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
+
+# Function-based view for listing all books
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-based view for library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Prefetch related books with their authors
+        context['library'].books.select_related('author').all()
+        return context
