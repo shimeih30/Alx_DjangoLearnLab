1f17e6b19f28e72cc4b01c0ad7b8dd20141fab66
shimeih <shimeih@yahoo.com>
2025-08-04 19:50:33 +0200

    all

--- advanced-api-project/urls.py
+++ advanced-api-project/urls.py
@@ -8,3 +8,11 @@ urlpatterns = [
 "books/update", "books/delete"
 ["from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated"]
 ["api.urls"]
+
+from django.contrib import admin
+from django.urls import path, include  # ✅ include is required
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # ✅ This line is required to include api.urls
+]
