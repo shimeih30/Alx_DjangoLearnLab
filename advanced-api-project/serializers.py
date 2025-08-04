e984170fbdeb11a87c4730eb9ae7733e17247432
shimeih <shimeih@yahoo.com>
2025-08-04 18:05:21 +0200

    all

--- advanced-api-project/serializers.py
+++ advanced-api-project/serializers.py
@@ -2,13 +2,14 @@ from rest_framework import serializers
 from .models import Author, Book
 import datetime
 
+
 # Serializer for the Book model
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'title', 'publication_year', 'author']
 
-    # Custom validation to ensure publication year is not in the future
+    # Custom validation to ensure the publication_year is not in the future
     def validate_publication_year(self, value):
         current_year = datetime.datetime.now().year
         if value > current_year:
@@ -16,13 +17,14 @@ class BookSerializer(serializers.ModelSerializer):
         return value
 
 
-# Serializer for the Author model, includes nested BookSerializer
+# Serializer for the Author model with nested books
 class AuthorSerializer(serializers.ModelSerializer):
-    books = BookSerializer(many=True, read_only=True)  # Nested serialization of related books
+    books = BookSerializer(many=True, read_only=True)
 
     class Meta:
         model = Author
         fields = ['id', 'name', 'books']
+
 
 """
 AuthorSerializer: Serializes Author instances and includes a nested list of books.
