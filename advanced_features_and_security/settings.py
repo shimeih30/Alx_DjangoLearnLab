a56b9811bcf21f9d460006bb34e5a75650f6b02e
shimeih <shimeih@yahoo.com>
2025-08-04 22:07:49 +0200

    all

--- advanced_features_and_security/settings.py
+++ advanced_features_and_security/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bookshelf',
     'relationship_app',
+    'accounts',
 
 ]
 
@@ -100,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 
 # Internationalization
