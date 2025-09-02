c2bc1365be348e01ea9c97e83dbf5ff389d998d4
shimeih <shimeih@yahoo.com>
2025-09-03 00:54:29 +0200

    great

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -20,3 +20,11 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+# api_project/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Include the api app URLs
+]
